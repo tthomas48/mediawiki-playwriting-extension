c146d7b2d1671a830682d30234085809af4689ef
Tim Thomas <tim@buyplaytix.com>
2012-02-22 20:47:40 -0600

    added code

---
+++ Scene.php
@@ -0,0 +1,31 @@
+<?php
+ # Example WikiMedia extension
+ # with WikiMedia's extension mechanism it is possible to define 
+ # new tags of the form 
+ # <TAGNAME> some text </TAGNAME>
+ # the function registered by the extension gets the text between the 
+ # tags as input and can transform it into arbitrary HTML code.
+ # Note: The output is not interpreted as WikiText but directly 
+ #       included in the HTML output. So Wiki markup is not supported.
+ # To activate the extension, include it from your LocalSettings.php
+ # with: include("extensions/ExampleExt.php"); 
+ $wgExtensionFunctions[] = "wfScene";
+ 
+ function wfScene() {
+     global $wgParser;
+     # register the extension with the WikiText parser
+     # the first parameter is the name of the new tag. 
+     # In this case it defines the tag <example> ... </example>
+     # the second parameter is the callback function for 
+     # processing the text between the tags
+     $wgParser->setHook( "scene", "renderScene" );
+ }
+ 
+ # The callback function for converting the input text to HTML output
+ function renderScene( $input, $argv ) {
+     # $argv is an array containing any arguments passed to the extension like <example argument="foo" bar>..
+     $output = "<div class=\"scene\">$input</div>";
+     return $output;
+ }
+ ?>
+
